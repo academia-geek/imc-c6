8f058b1ed2d6b71f7ded7da5f348e4180b2b09fc
devagiloscarmesa <oscar.mesa@agileinnova.org>
2021-04-27 13:00:00 -0500

    cambios

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+    
+module.exports = {
+  entry: './src/app/index.js',
+  mode : 'development', 
+  devServer : {
+    port: 9000,
+    open : true
+  }, 
+  output: {
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+        template: './src/index.html', 
+        minify:  {
+            collapseWhitespace: true,
+            removeComments: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            useShortDoctype: true
+          }
+          
+    })
+  ]
+
+};
